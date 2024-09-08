3deadb6959d99c94f58381250081747f1442265f
cangeroe7 <cangeroe7@gmail.com>
2024-09-08 15:28:28 -0500

    added init.lua file

---
+++ init.lua
@@ -0,0 +1,18 @@
+require("config.lazy")
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd.colorscheme("catppuccin")
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.undofile = true
+
+vim.opt.ignorecase = true
+
+vim.opt.smartcase = true
+
+
