c04f7949d4546bd2af5ddf3ac06bac98e1707d55
cangeroe7 <cangeroe7@gmail.com>
2025-03-18 14:04:25 -0500

    fixed auto formatting on save

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,23 +1,33 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
-	},
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
---				null_ls.builtins.diagnostics.mypy,
-				null_ls.builtins.formatting.black,
-				--null_ls.builtins.diagnostics.pylint,
-				--null_ls.builtins.diagnostics.flake8,
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        --				null_ls.builtins.diagnostics.mypy,
+        null_ls.builtins.formatting.black,
+        --null_ls.builtins.diagnostics.pylint,
+        --null_ls.builtins.diagnostics.flake8,
         null_ls.builtins.formatting.prettier.with({
-          filetypes = { "javascript", "typescript", "css", "scss", "html", "json", "yaml", "markdown", "graphql" }
+          filetypes = {
+            "javascript",
+            "typescript",
+            "css",
+            "scss",
+            "html",
+            "json",
+            "yaml",
+            "markdown",
+            "graphql",
+          },
         }),
-				--require("none-ls.diagnostics.ruff"),
-			},
-		})
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+        --require("none-ls.diagnostics.ruff"),
+      },
+    })
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }
